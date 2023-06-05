f65c989f8ebc6f9fe57205a3a7f0750180ed37a8
artsiom <artsiomzhuchkou2401@gmail.com>
2023-06-05 13:47:07 +0300

    chore: rename images

--- containers/images.js
+++ containers/images.js
@@ -1,8 +1,8 @@
 const images = {
     'idle_control': ['5scontrol/idle_python:v0.3.7', '5scontrol/idle_python:v0.3.9'],
     'min_max_control': ['5scontrol/min_max_python:v0.3.6', '5scontrol/min_max_python:v0.3.7', '5scontrol/min_max_python:v0.3.8', '5scontrol/min_max_python:v0.3.10'],
-    'operation_control': ['5scontrol/operation_control:v0.3.1'],
-    'machine_control': ['5scontrol/machine_control:v0.1.0'],
+    'operation_control': ['5scontrol/operation_control_js:v0.3.1'],
+    'machine_control': ['5scontrol/machine_control_js:v0.1.0'],
 }
 
 module.exports = {images}
