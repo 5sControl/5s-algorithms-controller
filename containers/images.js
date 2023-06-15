243fe9fe9a214fece246a42ccde42a66a1fb3583
Artsiom <artsiomzhuchkou2401@gmail.com>
2023-06-15 14:37:41 +0300

    chore: edited idle version

--- containers/images.js
+++ containers/images.js
@@ -1,5 +1,5 @@
 const images = {
-    'idle_control': ['5scontrol/idle_python:v0.3.7', '5scontrol/idle_python:v0.3.9', '5scontrol/idle_python:v0.3.10', '5scontrol/idle_python:v0.3.11'],
+    'idle_control': ['5scontrol/idle_python:v0.3.7', '5scontrol/idle_python:v0.3.9', '5scontrol/idle_python:v0.3.10', '5scontrol/idle_python:v0.3.11-stable'],
     'min_max_control': ['5scontrol/min_max_python:v0.3.6', '5scontrol/min_max_python:v0.3.7', '5scontrol/min_max_python:v0.3.8', '5scontrol/min_max_python:v0.3.10', '5scontrol/min_max_python:v0.3.11', '5scontrol/min_max_python:v0.3.12'],
     'operation_control': ['5scontrol/operation_control_js:v1.0.0-rc.1', '5scontrol/operation_control_js:v1.0.0-rc.2'],
     'machine_control': ['5scontrol/machine_control_js:v0.1.0', '5scontrol/machine_control_python:v1.0.1'],
