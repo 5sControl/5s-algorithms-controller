fd3586a655b73b637d9e6c0dcae1cb2277760447
slanx <andraffal@gmail.com>
2023-06-21 14:42:58 +0300

    feat: add safety image

--- containers/images.js
+++ containers/images.js
@@ -1,8 +1,26 @@
 const images = {
-    'idle_control': ['5scontrol/idle_python:v0.3.7', '5scontrol/idle_python:v0.3.9', '5scontrol/idle_python:v0.3.10', '5scontrol/idle_python:v0.3.11-stable', '5scontrol/idle_python:v0.3.12'],
-    'min_max_control': ['5scontrol/min_max_python:v0.3.6', '5scontrol/min_max_python:v0.3.7', '5scontrol/min_max_python:v0.3.8', '5scontrol/min_max_python:v0.3.10', '5scontrol/min_max_python:v0.3.11', '5scontrol/min_max_python:v0.3.12', '5scontrol/min_max_python:v0.3.13'],
-    'operation_control': ['5scontrol/operation_control_js:v1.0.0-rc.1', '5scontrol/operation_control_js:v1.0.0-rc.2'],
-    'machine_control': ['5scontrol/machine_control_js:v0.1.0', '5scontrol/machine_control_python:v1.0.1', '5scontrol/machine_control_python:v1.1.1'],
-}
+  idle_control: [
+    '5scontrol/idle_python:v0.3.7',
+    '5scontrol/idle_python:v0.3.9',
+    '5scontrol/idle_python:v0.3.10',
+    '5scontrol/idle_python:v0.3.11-stable',
+    '5scontrol/idle_python:v0.3.12',
+  ],
+  min_max_control: [
+    '5scontrol/min_max_python:v0.3.6',
+    '5scontrol/min_max_python:v0.3.7',
+    '5scontrol/min_max_python:v0.3.8',
+    '5scontrol/min_max_python:v0.3.10',
+    '5scontrol/min_max_python:v0.3.11',
+    '5scontrol/min_max_python:v0.3.12',
+    '5scontrol/min_max_python:v0.3.13',
+  ],
+  operation_control: [
+    '5scontrol/operation_control_js:v1.0.0-rc.1',
+    '5scontrol/operation_control_js:v1.0.0-rc.2',
+  ],
+  machine_control: ['5scontrol/machine_control_js:v0.1.0'],
+  safety_control_ear_protection: ['5scontrol/safety_control_ear_protection:v1.0.0'],
+};
 
-module.exports = {images}
+module.exports = { images };
