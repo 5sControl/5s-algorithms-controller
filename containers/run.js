d38d9c2aa34daed16dc951c803ba1989965f74bf
artsiom <artsiomzhuchkou2401@gmail.com>
2023-06-23 15:13:50 +0300

    cpu quota

--- containers/run.js
+++ containers/run.js
@@ -23,6 +23,7 @@ const startContainer = async (image = 'test_ref', name = 'test_ref', envVars = [
                         ReadOnly: false
                     }
                 ],
+                CpuQuota: 50000,
                 NetworkMode: 'host'
             }
         })
