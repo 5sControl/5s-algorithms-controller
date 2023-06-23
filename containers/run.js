ba61a5a42367e98693e2b8c73c0793f84ae1df8e
artsiom <artsiomzhuchkou2401@gmail.com>
2023-06-23 15:20:44 +0300

    cpu set cpus

--- containers/run.js
+++ containers/run.js
@@ -23,7 +23,7 @@ const startContainer = async (image = 'test_ref', name = 'test_ref', envVars = [
                         ReadOnly: false
                     }
                 ],
-                CpuQuota: 50000,
+                CpuSetCpus: 0.5,
                 NetworkMode: 'host'
             }
         })
