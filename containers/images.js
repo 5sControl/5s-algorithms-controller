2ce03a43aa6e76726d8b554a312505c5945f3764
artsiom <artsiomzhuchkou2401@gmail.com>
2023-07-12 12:11:02 +0300

    chore: new versions

--- containers/images.js
+++ containers/images.js
@@ -1,8 +1,8 @@
 const images = {
-    'idle_control': ['5scontrol/idle_python:v0.3.7', '5scontrol/idle_python:v0.3.9', '5scontrol/idle_python:v0.3.10', '5scontrol/idle_python:v0.3.11-stable', '5scontrol/idle_python:v0.3.13', '5scontrol/idle_python:v0.3.15', '5scontrol/idle_python:v0.4.0'],
-    'min_max_control': ['5scontrol/min_max_python:v0.3.6', '5scontrol/min_max_python:v0.3.7', '5scontrol/min_max_python:v0.3.8', '5scontrol/min_max_python:v0.3.10', '5scontrol/min_max_python:v0.3.11', '5scontrol/min_max_python:v0.3.12', '5scontrol/min_max_python:v0.3.16', '5scontrol/min_max_python:v0.4.0'],
-    'operation_control': ['5scontrol/operation_control_js:v1.0.0-rc.1', '5scontrol/operation_control_js:v1.0.0-rc.3', '5scontrol/operation_control_js:v1.0.0-rc.4'],
-    'machine_control': ['5scontrol/machine_control_js:v0.1.0', '5scontrol/machine_control_python:v1.0.1', '5scontrol/machine_control_python:v1.1.1', '5scontrol/machine_control_python:v1.2.2']
+    'idle_control': ['5scontrol/idle_python:v0.3.7', '5scontrol/idle_python:v0.3.9', '5scontrol/idle_python:v0.3.10', '5scontrol/idle_python:v0.3.11-stable', '5scontrol/idle_python:v0.3.13', '5scontrol/idle_python:v0.3.15', '5scontrol/idle_python:v0.4.1'],
+    'min_max_control': ['5scontrol/min_max_python:v0.3.6', '5scontrol/min_max_python:v0.3.7', '5scontrol/min_max_python:v0.3.8', '5scontrol/min_max_python:v0.3.10', '5scontrol/min_max_python:v0.3.11', '5scontrol/min_max_python:v0.3.12', '5scontrol/min_max_python:v0.3.16', '5scontrol/min_max_python:v0.4.1'],
+    'operation_control': ['5scontrol/operation_control_js:v1.0.0-rc.1', '5scontrol/operation_control_js:v1.0.0-rc.3', '5scontrol/operation_control_js:v1.0.0-rc.5'],
+    'machine_control': ['5scontrol/machine_control_js:v0.1.0', '5scontrol/machine_control_python:v1.0.1', '5scontrol/machine_control_python:v1.1.1', '5scontrol/machine_control_python:v1.2.3']
 }
 
 module.exports = {images}
