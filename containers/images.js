ab73f2865fda6ecf6afe1c89fbc1e2e04e8115c5
artsiom <artsiomzhuchkou2401@gmail.com>
2023-07-21 12:36:06 +0300

    chore: added new version

--- containers/images.js
+++ containers/images.js
@@ -1,6 +1,6 @@
 const images = {
     'idle_control': ['5scontrol/idle_python:v0.3.7', '5scontrol/idle_python:v0.3.9', '5scontrol/idle_python:v0.3.10', '5scontrol/idle_python:v0.3.11-stable', '5scontrol/idle_python:v0.3.13', '5scontrol/idle_python:v0.3.15', '5scontrol/idle_python:v0.4.3'],
-    'min_max_control': ['5scontrol/min_max_python:v0.3.6', '5scontrol/min_max_python:v0.3.7', '5scontrol/min_max_python:v0.3.8', '5scontrol/min_max_python:v0.3.10', '5scontrol/min_max_python:v0.3.11', '5scontrol/min_max_python:v0.3.12', '5scontrol/min_max_python:v0.3.16', '5scontrol/min_max_python:v0.4.3'],
+    'min_max_control': ['5scontrol/min_max_python:v0.3.6', '5scontrol/min_max_python:v0.3.7', '5scontrol/min_max_python:v0.3.8', '5scontrol/min_max_python:v0.3.10', '5scontrol/min_max_python:v0.3.11', '5scontrol/min_max_python:v0.3.12', '5scontrol/min_max_python:v0.3.16', '5scontrol/min_max_python:v0.4.4'],
     'operation_control': ['5scontrol/operation_control_js:v1.0.0-rc.1', '5scontrol/operation_control_js:v1.0.0-rc.3', '5scontrol/operation_control_js:v1.0.0-rc.6'],
     'machine_control': ['5scontrol/machine_control_js:v0.1.0', '5scontrol/machine_control_python:v1.0.1', '5scontrol/machine_control_python:v1.1.1', '5scontrol/machine_control_python:v1.2.6']
 }
