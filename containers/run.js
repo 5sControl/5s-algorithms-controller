ed81ec15174f5d6fa3575631edaa2e48577d0713
artsiom <artsiomzhuchkou2401@gmail.com>
2023-08-09 16:48:41 +0300

    feat: added task manager

--- containers/run.js
+++ containers/run.js
@@ -1,4 +1,3 @@
-const {images} = require("./images");
 const Docker = require('node-docker-api').Docker;
 const docker = new Docker({socketPath: '/var/run/docker.sock'})
 
@@ -76,8 +75,92 @@ const removeContainers = async (images) => {
     }
 };
 
-const runMinMaxModel = async () => {
+async function readContainerStats(container) {
+    try {
+        const statsStream = await container.stats();
+        const chunks = []
+        return new Promise((resolve, reject) => {
+            statsStream
+                .on('data', (chunk) => {
+                    if (chunks.length === 2) {
+                        statsStream.destroy();
+                        const currentStats = JSON.parse(chunks[1].toString());
+                        const previousStats = JSON.parse(chunks[0].toString());
+                        const data = {currentStats, previousStats}
+                        resolve(data);
+                    } else {
+                        chunks.push(chunk)
+                    }
+                })
+        });
+    } catch (err) {
+        console.error('Error:', err);
+        throw err;
+    }
+}
 
+async function readContainerLogs(container) {
+    try {
+        // const stream = await container.logs({follow: true, stdout: true, stderr: true})
+        return new Promise((resolve, reject) => {
+            container.logs({follow: true, stdout: true, stderr: true}).then((stream) => {
+                stream.on('data', (chunk) => {
+                    resolve(chunk.toString())
+                });
+            }).catch((err) => {
+                console.error('readContainerLogs error:', err);
+                resolve(err);
+            });
+        });
+    } catch (err) {
+        console.error('Error:', err);
+        throw err;
+    }
+}
+
+async function readContainerStatus(container) {
+    try {
+        let status = await container.status();
+        status = status.data.State.Status;
+        return status;
+    } catch (err) {
+        console.error('Error:', err);
+        throw err;
+    }
+}
+
+function calculateContainerCpuLoad(currentCpuStats, previousCpuStats) {
+    const currentTotalUsage = currentCpuStats.cpu_usage.total_usage;
+    const previousTotalUsage = previousCpuStats.cpu_usage.total_usage;
+
+    const currentSystemUsage = currentCpuStats.system_cpu_usage;
+    const previousSystemUsage = previousCpuStats.system_cpu_usage;
+
+    const cpuDelta = currentTotalUsage - previousTotalUsage;
+    const systemDelta = currentSystemUsage - previousSystemUsage;
+    const {online_cpus} = currentCpuStats;
+
+    const cpuLoadPercentage = (cpuDelta / systemDelta) * online_cpus * 100;
+
+    return cpuLoadPercentage.toFixed(2);
+}
+
+const getContainersStats = async (algorithms, pythonAlgorithms) => {
+    const algorithmsDataToSend = {};
+    for (const alg in algorithms) {
+        const {version, image, algorithm, camera_url} = algorithms[alg];
+        const container = pythonAlgorithms[camera_url][algorithm];
+        const status = await readContainerStatus(container);
+        let {previousStats, currentStats} = await readContainerStats(container);
+        const {memory_stats} = currentStats;
+        let ram = memory_stats.usage / 1000000; // to mb;
+        let cpu = calculateContainerCpuLoad(currentStats.cpu_stats, previousStats.cpu_stats);
+        cpu = cpu + '%';
+        ram = ram.toFixed(0) + 'M'
+        const additionalData = {cpu, ram, status}
+        algorithmsDataToSend[alg] = {...algorithms[alg], ...additionalData}
+    }
+    return algorithmsDataToSend;
 };
 
-module.exports = {startContainer, removeContainer, removeContainerByImage, removeContainers}
+module.exports = {startContainer, removeContainer, removeContainerByImage, removeContainers, getContainersStats}
